c734e7432b5614a89bdaa78c6640a0a4a2595b7c
mxxkz <jbmook@outlook.co.th>
2022-10-10 00:09:36 +0700

    Update patient&doctor profile

--- components/DoctorInfo.js
+++ components/DoctorInfo.js
@@ -6,46 +6,86 @@ import {
   Input,
   Image,
   Divider,
-} from "@chakra-ui/react";
-import Colour from "../Colour";
-import GlobalStyle from "../Style";
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from '@chakra-ui/react'
+import Colour from '../Colour'
+import { chakra } from '@chakra-ui/react'
 
 export default () => {
   let headText = {
-    color: "#000",
-    fontFamily: "Lato",
-    fontSize: "20px",
-    fontWeight: "bold",
-    textAlign: "left",
-    margin: "20px 0 20px",
-  };
-  let imageBox = {
-    justifyContent: "center",
-    alignItems: "center",
-  };
+    color: '#000',
+    fontFamily: 'Lato',
+    fontSize: '20px',
+    fontWeight: 'bold',
+    textAlign: 'left',
+    margin: '20px 0 20px',
+  }
+  let textBox = {
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
   let boxStyle = {
-    width: "60%",
-    alignItems: "center",
-    margin: "Auto",
-  };
+    width: '60%',
+    alignItems: 'center',
+    margin: 'Auto',
+  }
   let boxStyle2 = {
-    marginTop: "20px",
-    marginBottom: "20px",
-    boxShadow: "md",
-    borderRadius: "12px",
-    backgroundColor: "#fff",
-    padding: "20px 30px 30px 20px",
-  };
+    marginTop: '20px',
+    marginBottom: '20px',
+    borderRadius: '24px',
+    backgroundColor: '#fff',
+    padding: '30px 30px 30px 20px',
+    filter: 'drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.15))',
+  }
   let imgStyle = {
-    width: "160px",
-    height: "160px",
-    borderRadius: "50%",
-    objectFit: "cover",
-    margin: "16px 16px 16px 16px",
-  };
+    width: '160px',
+    height: '160px',
+    borderRadius: '50%',
+    objectFit: 'cover',
+    margin: '16px 16px 16px 16px',
+  }
   let infoBox = {
-    gap: "20px",
-  };
+    gap: '20px',
+  }
+  let medicalInfoBox = {
+    justifyContent: 'center',
+    flexDirection: 'column',
+    padding: '5px 10px 14px 10px',
+    gap: '10px',
+  }
+  let tabpanelStyle = {
+    backgroundColor: Colour.white,
+    border: '1px solid',
+    borderColor: Colour.lightGrey,
+    borderRadius: '0 0 24px 24px',
+    padding: '20px 20px',
+    filter: 'drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.15))',
+  }
+  let tabStyle = {
+    borderRadius: '12px 12px 0 0',
+    backgroundColor: Colour.lightBlue,
+  }
+  let mediumText = {
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '16px',
+    fontWeight: 'medium',
+  }
+  let textStyle = {
+    fontFamily: 'Lato',
+    fontSize: '20px',
+    fontWeight: 'bold',
+    color: Colour.white,
+  }
+  let regularText = {
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '16px',
+    fontWeight: 'regular',
+    color: Colour.darkGrey,
+  }
 
   return (
     <Box sx={boxStyle}>
@@ -53,8 +93,14 @@ export default () => {
       <Box sx={boxStyle2}>
         <Flex sx={infoBox}>
           <Box>
-            <Flex sx={imageBox}>
+            <Box>
               <Image sx={imgStyle} src="/images/profile.JPG" />
+            </Box>
+            <Flex sx={textBox}>
+              <Text>
+                <chakra.span sx={mediumText}>DOB: </chakra.span>
+                <chakra.span sx={regularText}>29 January 1989</chakra.span>
+              </Text>
             </Flex>
           </Box>
           <Box>
@@ -63,41 +109,54 @@ export default () => {
           <Box flex="1">
             <SimpleGrid columns={2} gap={5}>
               <Box>
-                <Text sx={GlobalStyle.mediumText}>First name</Text>
+                <Text sx={mediumText}>First name</Text>
                 <Input />
               </Box>
               <Box>
-                <Text sx={GlobalStyle.mediumText}>Last name</Text>
+                <Text sx={mediumText}>Last name</Text>
                 <Input />
               </Box>
               <Box>
-                <Text sx={GlobalStyle.mediumText}>Username</Text>
+                <Text sx={mediumText}>Username</Text>
                 <Input />
               </Box>
               <Box>
-                <Text sx={GlobalStyle.mediumText}>Password</Text>
+                <Text sx={mediumText}>Password</Text>
                 <Input />
               </Box>
               <Box>
-                <Text sx={GlobalStyle.mediumText}>Contact</Text>
+                <Text sx={mediumText}>Contact</Text>
                 <Input />
               </Box>
               <Box>
-                <Text sx={GlobalStyle.mediumText}>Email</Text>
-                <Input />
-              </Box>
-              <Box>
-                <Text sx={GlobalStyle.mediumText}>Medical License</Text>
-                <Input />
-              </Box>
-              <Box>
-                <Text sx={GlobalStyle.mediumText}>Department</Text>
+                <Text sx={mediumText}>Email</Text>
                 <Input />
               </Box>
             </SimpleGrid>
           </Box>
         </Flex>
       </Box>
+      <Tabs variant="enclosed">
+        <TabList>
+          <Tab sx={tabStyle}>
+            <Text sx={textStyle}>Medical Information</Text>
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel sx={tabpanelStyle}>
+            <Flex sx={medicalInfoBox}>
+              <Box>
+                <Text sx={mediumText}>Medical License</Text>
+                <Input />
+              </Box>
+              <Box>
+                <Text sx={mediumText}>Department</Text>
+                <Input />
+              </Box>
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
-  );
-};
+  )
+}
