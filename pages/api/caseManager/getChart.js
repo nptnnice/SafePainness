b2d386bb3990390098eceb3c81a95abca39107f0
ppetchc <piyachartpet@gmail.com>
2022-12-10 01:38:17 +0700

    modify Chart

--- pages/api/caseManager/getChart.js
+++ pages/api/caseManager/getChart.js
@@ -3,7 +3,9 @@ import db from '../../../db'
 export default async function handler(req, res) {
   const caseID = req.headers.caseid
   let result = await db.query(
-    `SELECT "painScale"::int8, "datetime" FROM "public"."Record" WHERE "caseID"=$1 LIMIT 10`,
+    `SELECT * FROM (
+      SELECT "painScale"::int8, "datetime" FROM "public"."Record" WHERE "caseID"=$1 ORDER BY "datetime" DESC LIMIT 10
+    ) subquery ORDER BY "datetime" ASC`,
     [caseID]
   )
   res.json(result.rows)
