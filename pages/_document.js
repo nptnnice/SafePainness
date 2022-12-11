66d604fdb6264e237b4fa2340f8028e63a34b05c
nptnnice <nptnnice29@gmail.com>
2022-12-11 15:09:58 +0700

    edit head

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
-import { Html, Main, NextScript } from 'next/document'
-import Head from 'next/head'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
